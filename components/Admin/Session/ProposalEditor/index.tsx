ccf36e17f99a9330c7d42a99228a948ede587a1e
Aiji Uejima <aiji42@gmail.com>
2020-09-17 14:46:05 +0000

    軽微な修正

--- components/Admin/Session/ProposalEditor/index.tsx
+++ components/Admin/Session/ProposalEditor/index.tsx
@@ -13,30 +13,40 @@ const ProposalEditor: FC = (props) => {
   return (
     <ArrayInput source="proposals">
       <SimpleFormIterator>
-        <BooleanInput source="human" />
+        <BooleanInput source="human" label="ユーザ側" />
         <SelectInput
           source="content.type"
+          label="メッセージタイプ"
           choices={[
             { id: 'string', name: 'テキスト' },
             { id: 'form', name: 'フォーム' }
           ]}
         />
-        <NumberInput source="content.delay" initialValue={500} />
+        <NumberInput
+          source="content.delay"
+          initialValue={500}
+          label="ローディング時間(ms)"
+        />
         <FormDataConsumer>
           {({ scopedFormData, getSource }: any) =>
             scopedFormData.content?.type === 'string' ? (
-              <StringTypeEditor source={getSource('props.children')} />
+              <StringTypeEditor
+                source={getSource('props.children')}
+                label="メッセージ本文"
+              />
             ) : (
               <FormTypeEditor />
             )
           }
         </FormDataConsumer>
+        <TextInput source="before" label="before function" />
+        <TextInput source="after" label="after function" />
       </SimpleFormIterator>
     </ArrayInput>
   )
 }
 
-const StringTypeEditor: FC<{ source: string }> = (props) => {
+const StringTypeEditor: FC<{ source: string; label: string }> = (props) => {
   return <TextInput {...props} />
 }
 
