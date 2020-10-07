bc4bf923c7e1f7b14313f482b8a1dc5170f149cc
Aiji Uejima <aiji42@gmail.com>
2020-10-07 14:47:15 +0000

    新規作成フォームをそれっぽくする

--- components/Admin/Session/CreateWizard/index.tsx
+++ components/Admin/Session/CreateWizard/index.tsx
@@ -1,20 +1,29 @@
-import { FC } from 'react'
+import { FC, useState } from 'react'
 import {
   SimpleForm,
   TextInput,
   RadioButtonGroupInput,
   Create,
-  CreateProps
+  CreateProps,
+  useCreateController,
+  FormWithRedirect,
+  SimpleFormProps
 } from 'react-admin'
 
 import {
   Card,
+  Paper,
   CardContent,
+  CardMedia,
   CardActions,
+  CardActionArea,
   CardHeader,
   Grid,
-  Typography
+  Typography,
+  IconButton
 } from '@material-ui/core'
+
+import { RadioButtonUnchecked, CheckCircle } from '@material-ui/icons'
 
 const proposals = [
   { id: 'ec', name: 'ECサイト' },
@@ -22,49 +31,111 @@ const proposals = [
   { id: 'manual', name: 'マニュアル' }
 ]
 
-const CreateWizard: FC<CreateProps> = (props) => {
+const CreateWizard: FC<SimpleFormProps> = (props) => {
+  const [templateType, setTemplateType] = useState<
+    null | 'ec' | 'inquiry' | 'manual'
+  >(null)
   return (
-    <Create {...props}>
-      <Grid container spacing={4}>
-        <Grid item xs={12}>
-          <SimpleForm>
-            <TextInput source="title" />
-          </SimpleForm>
-        </Grid>
-        <Grid item xs={4}>
-          <Card>
-            <CardHeader title="ECサイト用" />
-            <CardContent>
-              <Typography paragraph>
-                名前や住所等の個人情報及び、支払い・配送情報等を獲得するためのテンプレートです。
-              </Typography>
-              <Typography paragraph>通販サイトでの導入に最適です。</Typography>
-            </CardContent>
-          </Card>
-        </Grid>
-        <Grid item xs={4}>
-          <Card>
-            <CardHeader title="お問い合わせフォーム用" />
-            <CardContent>
-              <Typography paragraph>
-                名前や住所等の個人情報及び、アンケート式の選択フォームや自由入力のテキストエリアを備えたテンプレートです。
-              </Typography>
-              <Typography paragraph>
-                お問い合わせフォームやお申込みフォームなどに最適です。
-              </Typography>
-            </CardContent>
-          </Card>
-        </Grid>
-        <Grid item xs={4}>
-          <Card>
-            <CardHeader title="マニュアル作成" />
-            <CardContent>
-              <Typography paragraph></Typography>
-            </CardContent>
-          </Card>
-        </Grid>
-      </Grid>
-    </Create>
+    <FormWithRedirect
+      {...props}
+      render={(formProps) => (
+        <form>
+          <Grid container component={Paper} spacing={4}>
+            <Grid item xs={5}>
+              <TextInput source="title" fullWidth />
+            </Grid>
+            <Grid item xs={7} />
+            <Grid item xs={4}>
+              <Card>
+                <CardActionArea onClick={() => setTemplateType('ec')}>
+                  <CardHeader
+                    title="ECサイト用"
+                    action={
+                      <IconButton color="primary">
+                        {templateType === 'ec' ? (
+                          <CheckCircle fontSize="large" />
+                        ) : (
+                          <RadioButtonUnchecked fontSize="large" />
+                        )}
+                      </IconButton>
+                    }
+                  />
+                  <CardMedia
+                    component="img"
+                    src="/mobile-online-shopping.jpg"
+                  />
+                  <CardContent>
+                    <Typography paragraph variant="body1">
+                      名前や住所等の個人情報及び、支払い・配送情報等を獲得するためのテンプレートです。
+                    </Typography>
+                    <Typography variant="body2" color="textSecondary">
+                      通販サイトでの導入に最適です。
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+              </Card>
+            </Grid>
+            <Grid item xs={4}>
+              <Card>
+                <CardActionArea onClick={() => setTemplateType('inquiry')}>
+                  <CardHeader
+                    title="お問い合わせフォーム用"
+                    action={
+                      <IconButton color="primary">
+                        {templateType === 'inquiry' ? (
+                          <CheckCircle fontSize="large" />
+                        ) : (
+                          <RadioButtonUnchecked fontSize="large" />
+                        )}
+                      </IconButton>
+                    }
+                  />
+                  <CardMedia
+                    component="img"
+                    src="/mobile-testing-concept.jpg"
+                  />
+                  <CardContent>
+                    <Typography paragraph variant="body1">
+                      名前や住所等の個人情報及び、アンケート式の選択フォームや自由入力のテキストエリアを備えたテンプレートです。
+                    </Typography>
+                    <Typography variant="body2" color="textSecondary">
+                      お問い合わせフォームやお申込みフォームなどに最適です。
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+              </Card>
+            </Grid>
+            <Grid item xs={4}>
+              <Card>
+                <CardActionArea onClick={() => setTemplateType('manual')}>
+                  <CardHeader
+                    title="マニュアル"
+                    action={
+                      <IconButton color="primary">
+                        {templateType === 'manual' ? (
+                          <CheckCircle fontSize="large" />
+                        ) : (
+                          <RadioButtonUnchecked fontSize="large" />
+                        )}
+                      </IconButton>
+                    }
+                  />
+                  <CardMedia
+                    component="img"
+                    src="/business-people-launches-rocket.jpg"
+                  />
+                  <CardContent>
+                    <Typography paragraph variant="body1">
+                      テンプレートを使用せず、いちから自由に作成できます。
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+              </Card>
+            </Grid>
+          </Grid>
+        </form>
+      )}
+    />
   )
 }
 
