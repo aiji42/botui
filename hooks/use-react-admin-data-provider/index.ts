96f7239dea26db67f392311510846ca6d4d1d2b5
Aiji Uejima <aiji42@gmail.com>
2020-11-12 14:31:56 +0000

    少しリファクタとファイル整理

---
+++ hooks/use-react-admin-data-provider/index.ts
@@ -0,0 +1,85 @@
+import {
+  CreateParams,
+  GetListParams,
+  GetOneParams,
+  UpdateParams
+} from 'react-admin'
+import { buildDataProvider } from 'react-admin-amplify'
+import { DataProvider } from 'react-admin-amplify/build/providers/DataProvider'
+import { Session } from '../../@types/session'
+import * as mutations from '../../src/graphql/mutations'
+import * as queries from '../../src/graphql/queries'
+
+const sessionParse = (data: Session<string, string, string>): Session => {
+  const proposals = data.proposals ? JSON.parse(data.proposals) : []
+  const theme = data.theme ? JSON.parse(data.theme) : {}
+  const images = data.images ? JSON.parse(data.images) : {}
+  return { ...data, proposals, theme, images }
+}
+
+const sessionFormat = (data: Session): Session<string, string, string> => {
+  const proposals = data.proposals ? JSON.stringify(data.proposals) : '[]'
+  const theme = data.theme ? JSON.stringify(data.theme) : '{}'
+  const images = data.images ? JSON.stringify(data.images) : '{}'
+  return { ...data, proposals, theme, images }
+}
+
+const defaultDataProvider = buildDataProvider({ queries, mutations })
+const dataProvider = {
+  ...defaultDataProvider,
+  getList: async (resource: string, params: GetListParams) => {
+    if (resource !== 'sessions')
+      return await defaultDataProvider.getList(resource, params)
+
+    const result = await defaultDataProvider.getList<
+      Session<string, string, string>
+    >(resource, params)
+    return {
+      ...result,
+      data: result.data.map(sessionParse)
+    }
+  },
+  getOne: async (resource: string, params: GetOneParams) => {
+    if (resource !== 'sessions')
+      return await defaultDataProvider.getOne(resource, params)
+
+    const result = await defaultDataProvider.getOne<
+      Session<string, string, string>
+    >(resource, params)
+    return {
+      ...result,
+      data: sessionParse(result.data)
+    }
+  },
+  update: async (resource: string, params: UpdateParams) => {
+    if (resource !== 'sessions')
+      return defaultDataProvider.update(resource, params)
+
+    const result = await defaultDataProvider.update<
+      Session<string, string, string>
+    >(resource, {
+      ...params,
+      data: sessionFormat(params.data)
+    })
+    return {
+      ...result,
+      data: sessionParse(result.data)
+    }
+  },
+  create: async (resource: string, params: CreateParams) => {
+    if (resource !== 'sessions')
+      return await defaultDataProvider.create(resource, params)
+
+    const result = await defaultDataProvider.create<
+      Session<string, string, string>
+    >(resource, { ...params, data: { ...params.data } })
+    return {
+      ...result,
+      data: sessionParse(result.data)
+    }
+  }
+} as DataProvider
+
+const useDataProvider = () => dataProvider
+
+export default useDataProvider
--- pages/admin.tsx
+++ pages/admin.tsx
@@ -1,18 +1,8 @@
 import Head from 'next/head'
 import { FC } from 'react'
 import { withAuthenticator } from '@aws-amplify/ui-react'
-import { buildDataProvider, buildAuthProvider } from 'react-admin-amplify'
-import {
-  Resource,
-  Admin,
-  GetOneParams,
-  GetListParams,
-  UpdateParams,
-  DataProvider,
-  CreateParams
-} from 'react-admin'
-import * as mutations from '../src/graphql/mutations'
-import * as queries from '../src/graphql/queries'
+import { buildAuthProvider } from 'react-admin-amplify'
+import { Resource, Admin } from 'react-admin'
 import {
   SessionList,
   SessionEdit,
@@ -21,81 +11,12 @@ import {
 import japaneseMessages from '@bicstone/ra-language-japanese'
 import polyglotI18nProvider from 'ra-i18n-polyglot'
 import dynamic from 'next/dynamic'
-import { Session } from '../@types/session'
+import useDataProvider from '../hooks/use-react-admin-data-provider'
 
 const i18nProvider = polyglotI18nProvider(() => japaneseMessages)
 
-const sessionParse = (data: Session<string, string, string>): Session => {
-  const proposals = data.proposals ? JSON.parse(data.proposals) : []
-  const theme = data.theme ? JSON.parse(data.theme) : {}
-  const images = data.images ? JSON.parse(data.images) : {}
-  return { ...data, proposals, theme, images }
-}
-
-const sessionFormat = (data: Session): Session<string, string, string> => {
-  const proposals = data.proposals ? JSON.stringify(data.proposals) : '[]'
-  const theme = data.theme ? JSON.stringify(data.theme) : '{}'
-  const images = data.images ? JSON.stringify(data.images) : '{}'
-  return { ...data, proposals, theme, images }
-}
-
-const defaultDataProvider = buildDataProvider({ queries, mutations })
-const dataProvider = {
-  ...defaultDataProvider,
-  getList: async (resource: string, params: GetListParams) => {
-    if (resource !== 'sessions')
-      return await defaultDataProvider.getList(resource, params)
-
-    const result = await defaultDataProvider.getList<
-      Session<string, string, string>
-    >(resource, params)
-    return {
-      ...result,
-      data: result.data.map(sessionParse)
-    }
-  },
-  getOne: async (resource: string, params: GetOneParams) => {
-    if (resource !== 'sessions')
-      return await defaultDataProvider.getOne(resource, params)
-
-    const result = await defaultDataProvider.getOne<
-      Session<string, string, string>
-    >(resource, params)
-    return {
-      ...result,
-      data: sessionParse(result.data)
-    }
-  },
-  update: async (resource: string, params: UpdateParams) => {
-    if (resource !== 'sessions')
-      return defaultDataProvider.update(resource, params)
-
-    const result = await defaultDataProvider.update<
-      Session<string, string, string>
-    >(resource, {
-      ...params,
-      data: sessionFormat(params.data)
-    })
-    return {
-      ...result,
-      data: sessionParse(result.data)
-    }
-  },
-  create: async (resource: string, params: CreateParams) => {
-    if (resource !== 'sessions')
-      return await defaultDataProvider.create(resource, params)
-
-    const result = await defaultDataProvider.create<
-      Session<string, string, string>
-    >(resource, { ...params, data: { ...params.data } })
-    return {
-      ...result,
-      data: sessionParse(result.data)
-    }
-  }
-} as DataProvider
-
 const App: FC = () => {
+  const dataProvider = useDataProvider()
   return (
     <>
       <Head>
