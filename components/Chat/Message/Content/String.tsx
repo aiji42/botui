9014360b0f9a8a95bd51d4f7e6485be697fbfada
Aiji Uejima <aiji42@gmail.com>
2020-11-28 13:29:06 +0000

    チャットの文章が改行されるようにする #46

--- components/Chat/Message/Content/String.tsx
+++ components/Chat/Message/Content/String.tsx
@@ -1,14 +1,20 @@
 import { FC, useEffect } from 'react'
 import { StringType } from '@botui/types'
+import nl2br from 'react-nl2br'
 import { useMessageContext } from '../../../../hooks/use-message-context'
 
 const String: FC = () => {
   const { message, handleUpdate } = useMessageContext()
   const props = message.content.props as StringType
+  const { children, ...rest } = props
   useEffect(() => {
     handleUpdate && handleUpdate({ ...message, completed: true })
   }, [])
 
-  return <span {...props} />
+  return (
+    <span {...rest}>
+      {typeof children === 'string' ? nl2br(children) : children}
+    </span>
+  )
 }
 export default String
