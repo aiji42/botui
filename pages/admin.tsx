0e33bb08c6832a0eceb309837af581d71b1ee7bf
Aiji Uejima <aiji42@gmail.com>
2020-12-08 12:46:35 +0000

    ログインページを作成 & アカウント作成時の電話番号入力フィールドを排除

--- pages/admin.tsx
+++ pages/admin.tsx
@@ -1,41 +1,19 @@
 import Head from 'next/head'
 import { FC } from 'react'
-import {
-  withAuthenticator,
-  AmplifyAuthenticator,
-  AmplifySignUp
-} from '@aws-amplify/ui-react'
 import { buildAuthProvider } from 'react-admin-amplify'
-import { Resource, Admin, Login } from 'react-admin'
+import { Resource, Admin } from 'react-admin'
 import {
   SessionList,
   SessionEdit,
   SessionCreate
 } from '../components/Admin/Session'
+import Login from '../components/Admin/Login'
 import japaneseMessages from '@bicstone/ra-language-japanese'
 import polyglotI18nProvider from 'ra-i18n-polyglot'
 import dynamic from 'next/dynamic'
 import useDataProvider from '../hooks/use-react-admin-data-provider'
 
 const i18nProvider = polyglotI18nProvider(() => japaneseMessages)
-
-const authProvider = {
-  ...buildAuthProvider(),
-  checkAuth: async (): Promise<any> => {
-    try {
-      const a = await buildAuthProvider().checkAuth(null)
-      console.log(a)
-    } catch (e) {
-      console.log(e)
-      return { redirectTo: '/login' }
-    }
-    return { redirectTo: '/sessions' }
-  }
-}
-
-const MyLoginPage: FC = () => {
-  return <Login></Login>
-}
 
 const App: FC = () => {
   const dataProvider = useDataProvider()
@@ -47,9 +25,9 @@ const App: FC = () => {
       </Head>
       <Admin
         dataProvider={dataProvider}
-        authProvider={authProvider}
+        authProvider={buildAuthProvider()}
         i18nProvider={i18nProvider}
-        loginPage={MyLoginPage}
+        loginPage={Login}
       >
         <Resource
           name="sessions"
@@ -63,7 +41,6 @@ const App: FC = () => {
   )
 }
 
-// const AppWithAuthenticator = withAuthenticator(App, { usernameAlias: 'email' })
 export default dynamic(() => Promise.resolve(App), {
   ssr: false
 })
