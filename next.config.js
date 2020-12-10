1e3621eba7d936edc3f3afe7131cc097def2bff5
Aiji Uejima <aiji42@gmail.com>
2020-12-10 14:11:27 +0000

    wip

---
+++ next.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  webpack: (config) => ({
+    ...config,
+    entry: () =>
+      config.entry().then((entry) => ({
+        ...entry,
+        embeddedLibrary: './embedded'
+      }))
+  })
+}
