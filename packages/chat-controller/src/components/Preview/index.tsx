de2812e760916792fd77cdf116c5b33b971e20a0
Aiji Uejima <aiji42@gmail.com>
2021-01-17 14:28:41 +0000

    feat(chat-controller): create Preview

---
+++ packages/chat-controller/src/components/Preview/index.tsx
@@ -0,0 +1,32 @@
+import React, { FC, useState } from 'react'
+import { Controller } from '../'
+import { ChatConfig, Proposals } from '@botui/types'
+
+export const Preview: FC<{
+  proposals: Proposals
+  chatConfig: ChatConfig
+}> = (props) => {
+  const [iframeElement, setIframeElement] = useState<HTMLIFrameElement | null>(
+    null
+  )
+  const [initProposals] = useState<Proposals>(props.proposals)
+
+  return (
+    <>
+      <iframe
+        ref={setIframeElement}
+        src="/"
+        height="100%"
+        width="100%"
+        frameBorder="no"
+      />
+      {!!iframeElement?.contentWindow && initProposals.length && (
+        <Controller
+          targetWindow={iframeElement.contentWindow}
+          initProposals={initProposals}
+          chatConfig={props.chatConfig}
+        />
+      )}
+    </>
+  )
+}
