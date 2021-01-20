c2b6c6a6908ed7ccc5a164c3a7b0bfce6d4afc00
Aiji Uejima <aiji42@gmail.com>
2021-01-20 15:23:28 +0000

    fix(chat-host): change font

--- packages/chat-host/src/index.tsx
+++ packages/chat-host/src/index.tsx
@@ -6,11 +6,7 @@ import App from './Chat'
 const global = css`
   body {
     margin: 0;
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto',
-      'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans',
-      'Helvetica Neue', sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
+    font-family: "Noto Sans JP", sans-serif;
   }
 `
 
