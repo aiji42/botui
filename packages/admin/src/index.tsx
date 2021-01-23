c20913dcb964442715fe1ced9f745b133d7b19c3
Aiji Uejima <aiji42@gmail.com>
2021-01-23 03:25:55 +0000

    fix(admin): use environment variables to change the preview host

--- packages/admin/src/index.tsx
+++ packages/admin/src/index.tsx
@@ -2,6 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import Admin from './Admin'
+import { setPreviewConfig } from '@botui/chat-controller'
+
+setPreviewConfig({ chatHost: process.env.REACT_APP_BOTUI_HOST ?? 'http://localhost:3000/' })
 
 ReactDOM.render(
   <React.StrictMode>
