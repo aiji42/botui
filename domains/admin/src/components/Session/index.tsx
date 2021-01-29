de994e9843af01ce427cc6928883760834f59ec8
Aiji Uejima <aiji42@gmail.com>
2021-01-29 15:57:36 +0000

    feat(admin): add embedded script panel

--- domains/admin/src/components/Session/index.tsx
+++ domains/admin/src/components/Session/index.tsx
@@ -1,4 +1,4 @@
-import { FC, useCallback, useState, ChangeEvent } from 'react'
+import { FC } from 'react'
 import {
   Datagrid,
   List,
@@ -12,70 +12,13 @@ import {
 } from 'react-admin'
 import EditForm from './Edit'
 import CreateForm from './Create'
-import {
-  Grid,
-  Switch,
-  FormControlLabel,
-  Select,
-  MenuItem
-} from '@material-ui/core'
+import { EmbeddedScriptPanel } from './EmbeddedScriptPanel'
 
-type Size = 'auto' | 'widget' | 'full'
-
-const embeddedScript = (
-  sessionId: string,
-  size: Size,
-  defaultOpen: boolean
-) => `
-<script src="https://unpkg.com/@botui-domain/embedded" type="text/javascript">
-  new BotuiChat.default('${sessionId}',{defaultOpen:${defaultOpen},size:'${size}'}).start()
-</script>
-`
-
-const assignable = (arg: unknown): arg is Size =>
-  typeof arg === 'string' && ['auto', 'widget', 'full'].includes(arg)
-
-const SessionPanel: FC<{ id?: string }> = (props) => {
-  const [defaultOpen, setDefaultOpen] = useState<boolean>(false)
-  const [size, setSize] = useState<Size>('auto')
-  const handleChangeDefaultOpen = useCallback((...[, checked]: [unknown, boolean]) => setDefaultOpen(checked), [setDefaultOpen])
-  const handleChangeSize = useCallback(
-    (e: ChangeEvent<{ value: unknown }>) => assignable(e.target.value) && setSize(e.target.value),
-    [setSize]
-  )
-  return (
-    <Grid container>
-      <Grid item xs={12}>
-        <FormControlLabel
-          control={
-            <Select onChange={handleChangeSize} defaultValue={size}>
-              <MenuItem value="auto">おまかせ(画面幅で自動切り替え)</MenuItem>
-              <MenuItem value="full">全画面</MenuItem>
-              <MenuItem value="widget">ウィジェット</MenuItem>
-            </Select>
-          }
-          label="サイズ"
-        />
-      </Grid>
-      <Grid item xs={12}>
-        <FormControlLabel
-          control={
-            <Switch checked={defaultOpen} onChange={handleChangeDefaultOpen} />
-          }
-          label="即時起動"
-        />
-      </Grid>
-      <Grid item xs={12}>
-        <code>{embeddedScript(props.id ?? '', size, defaultOpen)}</code>
-      </Grid>
-    </Grid>
-  )
-}
 
 export const SessionList: FC = (props) => {
   return (
     <List {...props} bulkActionButtons={false} exporter={false}>
-      <Datagrid rowClick="edit" expand={<SessionPanel />}>
+      <Datagrid rowClick="edit" expand={<EmbeddedScriptPanel />}>
         <TextField source="title" sortable={false} />
         <BooleanField source="active" />
       </Datagrid>
