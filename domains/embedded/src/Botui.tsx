982d093eedc6fe24597033901983ed62ece3df2c
Aiji Uejima <aiji42@gmail.com>
2021-01-29 14:50:08 +0000

    wip

--- domains/embedded/src/Botui.tsx
+++ domains/embedded/src/Botui.tsx
@@ -4,25 +4,22 @@ import { render } from 'react-dom'
 
 export class Botui {
   sessionId: string
-  mountTarget: string
   size: SizeType
   defaultOpen: boolean
 
   constructor(
     sessionId: string,
-    mountTarget: string,
     config: { size?: SizeType; defaultOpen?: boolean } = {}
   ) {
     const { size = SIZE.Auto, defaultOpen = false } = config
     this.sessionId = sessionId
-    this.mountTarget = mountTarget
     this.size = size
     this.defaultOpen = defaultOpen
   }
 
   start() {
-    const target = document.querySelector(this.mountTarget)
-    if (!target) return
+    const target = document.createElement('div')
+    document.querySelector('body')?.appendChild(target)
 
     render(
       <Preview
