500c164e90edb1e9f2b6ecac214f7e0158d1cd11
Aiji Uejima <aiji42@gmail.com>
2021-01-30 13:39:31 +0000

    feat(scripts): add deal-awsexports.sh

---
+++ scripts/deal-awsexports.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+ln src/aws-exports.js main/src/aws-exports.js
+ln src/aws-exports.js packages/api/src/aws-exports.js
+ln src/aws-exports.js domains/admin/src/aws-exports.js
+ln src/aws-exports.js domains/chat/src/aws-exports.js
+ln src/aws-exports.js domains/embedded/src/aws-exports.js
