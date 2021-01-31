30f2f03279094289ae6b3b3a3322a4020ccd93de
Aiji Uejima <aiji42@gmail.com>
2021-01-31 14:19:31 +0000

    feat(admin): add launcher setting form

--- domains/admin/src/components/Session/Edit/index.tsx
+++ domains/admin/src/components/Session/Edit/index.tsx
@@ -2,6 +2,7 @@ import { FC } from 'react'
 import { SimpleFormProps, FormTab, TabbedForm } from 'react-admin'
 import SessionFormInner from './SessionFormInner'
 import ProposalViewerAndEditor from './ProposalsViewerAndEditor'
+import ChatLauncherFormInner from './ChatLauncherFormInner'
 
 type Props = Omit<SimpleFormProps, 'children'>
 
@@ -13,6 +14,9 @@ const Edit: FC<Props> = (props) => {
       </FormTab>
       <FormTab label="タイムライン">
         <ProposalViewerAndEditor />
+      </FormTab>
+      <FormTab label="ランチャー">
+        <ChatLauncherFormInner />
       </FormTab>
     </TabbedForm>
   )
