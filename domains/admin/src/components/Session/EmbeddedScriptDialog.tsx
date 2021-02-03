1a457c0de4a8dd21df46d1bae214642deac74314
Aiji Uejima <aiji42@gmail.com>
2021-02-03 13:47:31 +0000

    fix(admin): update EmbeddedScriptDialog

--- domains/admin/src/components/Session/EmbeddedScriptDialog.tsx
+++ domains/admin/src/components/Session/EmbeddedScriptDialog.tsx
@@ -4,19 +4,14 @@ import { Dialog, DialogContent, Button, Typography } from '@material-ui/core'
 import { Code } from '@material-ui/icons'
 import { Session } from '@botui/types'
 
-const embeddedScript = (
-  sessionId: string,
-  size: Session['launcher']['size'],
-  defaultOpen: boolean
-) => `<script src="https://unpkg.com/@botui-domain/embedded"></script>
-<script type="text/javascript">new BotuiChat.default('${sessionId}',{defaultOpen:${defaultOpen},size:'${size}'}).start()</script>`
+const embeddedScript = (sessionId: string) => `<script src="https://unpkg.com/@botui-domain/embedded"></script>
+<script type="text/javascript">new BotuiChat.default('${sessionId}').start()</script>`
 
 interface EmbeddedScriptDialogProps {
   session: Session
-  disabled?: boolean
 }
 
-const EmbeddedScriptDialog: FC<EmbeddedScriptDialogProps> = ({ session, disabled = false }) => {
+const EmbeddedScriptDialog: FC<EmbeddedScriptDialogProps> = ({ session }) => {
   const [open, setOpen] = useState(false)
   const handleClose = useCallback(() => setOpen(false), [setOpen])
   return (
@@ -24,7 +19,6 @@ const EmbeddedScriptDialog: FC<EmbeddedScriptDialogProps> = ({ session, disabled
       <Button
         startIcon={<Code />}
         onClick={() => setOpen(true)}
-        disabled={disabled}
       >
         公開用タグ
       </Button>
@@ -34,11 +28,7 @@ const EmbeddedScriptDialog: FC<EmbeddedScriptDialogProps> = ({ session, disabled
             下記のコードをチャットを起動させたいページのHEADタグ内に埋め込んでください。
           </Typography>
           <SyntaxHighlighter language="text">
-            {embeddedScript(
-              session.id,
-              session.launcher.size,
-              session.launcher.defaultOpen
-            )}
+            {embeddedScript(session.id)}
           </SyntaxHighlighter>
         </DialogContent>
       </Dialog>
