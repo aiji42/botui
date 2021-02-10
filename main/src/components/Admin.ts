db0aa8adf09ad91003467eac7fcd1f283e0719a6
Aiji Uejima <aiji42@gmail.com>
2021-02-10 14:53:43 +0000

    chore(main): create handler multiple redirect urls

--- main/src/components/Admin.ts
+++ main/src/components/Admin.ts
@@ -1,5 +1,15 @@
 import Admin, { initAmplifyConfig } from '@botui-domain/admin'
-import aws_exports from '../aws-exports'
-initAmplifyConfig(aws_exports)
+import awsconfig from '../aws-exports'
+
+if (awsconfig.oauth.redirectSignIn.includes(',')) {
+  const filterHost = (url: string) =>
+    new URL(url).host === window.location.host
+  awsconfig.oauth.redirectSignIn =
+    awsconfig.oauth.redirectSignIn.split(',').filter(filterHost).shift() ?? ''
+  awsconfig.oauth.redirectSignOut =
+    awsconfig.oauth.redirectSignOut.split(',').filter(filterHost).shift() ?? ''
+}
+
+initAmplifyConfig(awsconfig)
 
 export default Admin
